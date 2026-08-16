70df14e82fa4030fd7a940e19aa9a0e6f21df8dc
olyvia-eng <olyvia@fieldscape.ca>
2026-08-16 15:56:25 -0400

    stage 1

--- index.tsx
+++ index.tsx
@@ -1,14 +1,9 @@
 import { registerRootComponent } from 'expo';
 import { Text } from 'react-native';
-import { featureFlags } from 'react-native-screens';
-
-featureFlags.experiment.synchronousScreenUpdatesEnabled = true;
-featureFlags.experiment.synchronousHeaderConfigUpdatesEnabled = true;
-featureFlags.experiment.synchronousHeaderSubviewUpdatesEnabled = true;
-featureFlags.experiment.iosPreventReattachmentOfDismissedScreens = true;
+import 'react-native-screens';
 
 function App() {
-  return <Text>OliveOps Screens Module OK</Text>;
+  return <Text>OliveOps Screens Import OK</Text>;
 }
 
 registerRootComponent(App);
