46f6409365df66edd86f23ee35f84c6293a4e28c
olyvia-eng <olyvia@fieldscape.ca>
2026-08-16 09:14:35 -0400

    next test

--- index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { registerRootComponent } from 'expo';
-import { Text } from 'react-native';
-
-function App() {
-	return <Text>OliveOps Expo Root OK</Text>;
-}
-
-registerRootComponent(App);
---
+++ src/routerDiagnostic/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export default function IndexRoute() {
+  return <Text>OliveOps Router OK</Text>;
+}
